c4591acdded3cf1b6d7b6c279ac75473be13d306
aalvaroo7 <almarrom@myuax.com>
2023-12-12 23:33:59 +0100

    comit no me va el wifi de casa :(

--- ej4.cpp
+++ ej4.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <cstring>
+struct estudiante {;
+    int edad;
+    char nombre[50];
+    float promedio;
+};
+int main() {
+    typedef struct Estudiante Estudiante;
 
+    Estudiante estudiante3;
+    strcpy(estudiante3.nombre, "Pedro");
+    estudiante3.edad = 25;
+    estudiante3.promedio = 9.7;
+    return 0;
+}
