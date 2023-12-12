385fd472b8006e6bfa5b4864f1828758edbd5c91
aalvaroo7 <almarrom@myuax.com>
2023-12-13 00:08:17 +0100

    jaja me printeo 1

--- ej5.cpp
+++ ej5.cpp
@@ -1,12 +1,11 @@
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 struct estudiante {
     int edad;
-    char nombre[50];
     float promedio;
+    string nombre; // Agregado el campo "nombre" como un string
 };
 
 void imprimirEstudiante(estudiante est) {
@@ -17,6 +16,7 @@ int main() {
     struct estudiante estudiante1;
     estudiante1.edad = 16;
     estudiante1.promedio = 9.8;
+    estudiante1.nombre = "Juan"; // Asignando un valor al campo "nombre"
     imprimirEstudiante(estudiante1);
     return 0;
 }
