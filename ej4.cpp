a5316e6f387cb389a157f5a143929e992760cdad
aalvaroo7 <almarrom@myuax.com>
2023-12-12 23:35:12 +0100

    comit

--- ej4.cpp
+++ ej4.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <cstring>
-struct estudiante {;
+struct Estudiante {;
     int edad;
     char nombre[50];
     float promedio;
 };
 int main() {
     typedef struct Estudiante Estudiante;
-
     Estudiante estudiante3;
     strcpy(estudiante3.nombre, "Pedro");
     estudiante3.edad = 25;
