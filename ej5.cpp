dc895c30498ca0f539a83447989636bce77a2029
aalvaroo7 <almarrom@myuax.com>
2023-12-13 00:02:59 +0100

    comit

--- ej5.cpp
+++ ej5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+using namespace std;
 struct estudiante { ;
     int edad;
     char nombre[50];
@@ -9,6 +10,9 @@ void imprimirEstudiante(estudiante est) {
     printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", est.nombre, est.edad, est.promedio);
 }
 int main() {
-
+    struct estudiante estudiante1;
+    estudiante1.edad = 16;
+    estudiante1.promedio = 9.8;
+cout << imprimirEstudiante;
     return 0;
 }
