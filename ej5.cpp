bf4e429e9bc602710060397aaebc7c392743c362
aalvaroo7 <almarrom@myuax.com>
2023-12-13 00:06:43 +0100

    jaja me printeo 1

--- ej5.cpp
+++ ej5.cpp
@@ -1,18 +1,22 @@
+
 #include <iostream>
-#include <cstring>
+#include <iomanip>
 using namespace std;
-struct estudiante { ;
+
+struct estudiante {
     int edad;
     char nombre[50];
     float promedio;
 };
+
 void imprimirEstudiante(estudiante est) {
-    printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", est.nombre, est.edad, est.promedio);
+    cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << fixed << setprecision(2) << est.promedio << endl;
 }
+
 int main() {
     struct estudiante estudiante1;
     estudiante1.edad = 16;
     estudiante1.promedio = 9.8;
-cout << imprimirEstudiante;
+    imprimirEstudiante(estudiante1);
     return 0;
 }
