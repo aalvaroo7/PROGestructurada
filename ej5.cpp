8206d864ff2bb0636ec7ddf5d73ad1ce15b1e11f
aalvaroo7 <almarrom@myuax.com>
2023-12-13 00:12:01 +0100

    comit

--- ej5.cpp
+++ ej5.cpp
@@ -11,6 +11,9 @@ struct estudiante {
 void imprimirEstudiante(estudiante est) {
     cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << fixed << setprecision(2) << est.promedio << endl;
 }
+void modificarEstudiante(Estudiante *est) {
+    est->edad = 30;
+}
 
 int main() {
     struct estudiante estudiante1;
@@ -18,8 +21,6 @@ int main() {
     estudiante1.promedio = 9.8;
     estudiante1.nombre = "Juan"; // Asignando un valor al campo "nombre"
     imprimirEstudiante(estudiante1);
+    modificarEstudiante(&estudiante1);
     return 0;
 }
-//he definido el nombre como float porque si no se puede asignar un nombre a un array de tipo char y entonces
-//a la hora de mostrar un nombre pues muestra algo raro como una casa o algo asi
-//entonces cambio el tipo de variable a string y ya se puede asignar un nombre y mostrarlo correctamente
