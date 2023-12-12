6481caa1af083f90031339f166015c44518fa510
aalvaroo7 <almarrom@myuax.com>
2023-12-13 00:14:39 +0100

    comit

--- ej5.cpp
+++ ej5.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <cstring>
+#include <string>
 using namespace std;
 
 struct estudiante {
@@ -11,12 +13,15 @@ struct estudiante {
 void imprimirEstudiante(estudiante est) {
     cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << fixed << setprecision(2) << est.promedio << endl;
 }
+
 void modificarEstudiante(estudiante *est) {
     est->edad = 30;
 }
+
 void imprimirEstudiante2(estudiante *est) {
-    printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", est->nombre, est->edad, est->promedio);
+    cout << "Nombre: " << est->nombre << ", Edad: " << est->edad << ", Promedio: " << fixed << setprecision(2) << est->promedio << endl;
 }
+
 int main() {
     struct estudiante estudiante1;
     estudiante1.edad = 16;
